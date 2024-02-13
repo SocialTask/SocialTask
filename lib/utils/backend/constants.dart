301f1ec34a9b048cb1615b5d8c8b6a0bac337d5c
Andoni Iriso <62946099+andiricum2@users.noreply.github.com>
2024-02-13 10:55:39 +0100

    Update constants.dart

--- lib/utils/backend/constants.dart
+++ lib/utils/backend/constants.dart
@@ -1,3 +1,3 @@
 class Constants {
-  static const String baseUrl = 'fr-msr-1.halex.gg:25052';
+  static const String baseUrl = 'http://fr-msr-1.halex.gg:25052'; // Dev Server. Server status in https://socialtask.openstatus.dev
 }
