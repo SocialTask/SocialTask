0b3fad704e3535f938dee34fe47465468ed9c5e9
andiricum2 <andiricum2@gmail.com>
2024-02-14 19:35:02 +0100

    Update constants.dart

--- lib/utils/backend/constants.dart
+++ lib/utils/backend/constants.dart
@@ -1,3 +1,4 @@
 class Constants {
-  static const String baseUrl = 'http://fr-msr-1.halex.gg:25052'; // Dev Server. Server status in https://socialtask.openstatus.dev
+  static const String baseUrl =
+      'http://free2.stozu.net:8999'; // Dev Server. Server status in https://socialtask.openstatus.dev
 }
